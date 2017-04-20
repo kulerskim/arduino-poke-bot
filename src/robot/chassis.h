94ebfd2d4b191f6d1d5165bf05411379a097f482
Mariusz Kulerski <mariusz.kulerski@elpassion.pl>
2017-04-20 09:43:23 +0200

    Update motor pinout after rebuild

--- src/robot/chassis.h
+++ src/robot/chassis.h
@@ -10,12 +10,12 @@ class Chassis
     static const int DEFAULT_SPEED = 255;
     static const int STRAIGTH_MOVE_TIME = 1000;
     static const int STRAIGHT_TURN_TIME = 700;
-    static const int MOTOR_PIN_ENA = 5;
+    static const int MOTOR_PIN_ENA = 4;
     static const int MOTOR_PIN_IN1 = 2;
     static const int MOTOR_PIN_IN2 = 3;
-    static const int MOTOR_PIN_IN3 = 4;
+    static const int MOTOR_PIN_IN3 = 6;
     static const int MOTOR_PIN_IN4 = 7;
-    static const int MOTOR_PIN_ENB = 6;
+    static const int MOTOR_PIN_ENB = 5;
     Chassis();
     void forward();
     void backward();
